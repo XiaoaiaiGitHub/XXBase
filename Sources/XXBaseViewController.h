7868b74793c407c1c5e2d47a7ae81dbfb5d5b5d7
FaDeve <>
2019-04-11 12:11:04 +0800

    Initial XXBase

---
+++ Sources/XXBaseViewController.h
@@ -0,0 +1,17 @@
+//
+//  XXBaseViewController.h
+//  TestPod
+//
+//  Created by FaDeve on 2019/4/11.
+//  Copyright © 2019 J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XXBaseViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Sources/XXBaseViewController.m
@@ -0,0 +1,32 @@
+//
+//  XXBaseViewController.m
+//  TestPod
+//
+//  Created by FaDeve on 2019/4/11.
+//  Copyright © 2019 J. All rights reserved.
+//
+
+#import "XXBaseViewController.h"
+
+@interface XXBaseViewController ()
+
+@end
+
+@implementation XXBaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
